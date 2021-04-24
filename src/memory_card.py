2ec4d520bf32a326032f0325f57af138cecba0c7
Facundo Luciano Sanchez <flsanchez94@gmail.com>
2021-04-24 18:54:43 -0300

    starting with directory structure implementation

--- src/memory_card.py
+++ src/memory_card.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from icon import IconSet
 
 KB = 1024
@@ -9,6 +11,8 @@ class MemoryCard:
     self._load_file_contents()
     self._blocks = None
     self._generate_blocks()
+    self._directory_structure = None
+    self._generate_directory_structure()
 
   def _load_file_contents(self):
     expected_file_bytes_size = 128*KB
@@ -43,6 +47,35 @@ class MemoryCard:
     except UnicodeDecodeError:
       title = self._blocks[block_number]._frames[0]._frame_data[0x04:(0x04+32)].decode('shift-jis')
     return title
+
+  def _generate_directory_structure(self):
+    directory_frames = [
+      self._file_contents[
+        0x80*(frame_number):0x80*(frame_number+1)
+      ] for frame_number in range(1, 16)
+    ]
+    self._directory_structure = [
+      self._parse_directory_frame(frame) for frame in directory_frames
+    ]
+
+  def _parse_directory_frame(self, raw_data):
+    base_256 = lambda n: np.array([256**i for i in range(n)])
+    name = raw_data[0x0A:0x1E]
+    block_state = np.dot(np.array(list(raw_data[:0x04])), base_256(4))
+    block_size = np.dot(np.array(list(raw_data[0x04:0x08])), base_256(4))/KB
+    next_block = np.dot(np.array(list(raw_data[0x08:0x0A])), base_256(2))
+    return {
+      'name': name,
+      'block_state': block_state,
+      'block_size': block_size,
+      'next_block': next_block,
+    }
+
+class BlockAllocationConstants:
+  FIRST_BLOCK = 0x51
+  MIDDLE_BLOCK = 0x52
+  LAST_BLOCK = 0x53
+  ALL = {FIRST_BLOCK, MIDDLE_BLOCK, LAST_BLOCK}
 
 
 class Block:
