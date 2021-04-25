a32c57dfb896f0e8cc2cfa1de480ab9a9d53c20b
Facundo Luciano Sanchez <flsanchez94@gmail.com>
2021-04-25 11:30:49 -0300

    DirectoryBlock class to obtain directory structure info

--- src/memory_card.py
+++ src/memory_card.py
@@ -3,6 +3,8 @@ import numpy as np
 from icon import IconSet
 
 KB = 1024
+BLOCK_SIZE = 8*KB
+FRAME_SIZE = BLOCK_SIZE//64
 
 
 class MemoryCard:
@@ -11,8 +13,8 @@ class MemoryCard:
     self._load_file_contents()
     self._blocks = None
     self._generate_blocks()
-    self._directory_structure = None
-    self._generate_directory_structure()
+    self._directory_block = None
+    self._generate_directory_block()
 
   def _load_file_contents(self):
     expected_file_bytes_size = 128*KB
@@ -29,7 +31,7 @@ class MemoryCard:
   def _block_generator(self):
     block = 0
     while block < 16:
-      yield self._file_contents[block*8*KB:(block+1)*8*KB]
+      yield self._file_contents[block*BLOCK_SIZE:(block+1)*BLOCK_SIZE]
       block += 1
 
   def _generate_blocks(self):
@@ -48,28 +50,48 @@ class MemoryCard:
       title = self._blocks[block_number]._frames[0]._frame_data[0x04:(0x04+32)].decode('shift-jis')
     return title
 
+  def _generate_directory_block(self):
+    self._directory_block = DirectoryBlock(self._file_contents[:BLOCK_SIZE])
+
+
+class DirectoryBlock:
+
+  def __init__(self, raw_data):
+    self._block_raw_data = raw_data
+    self._directory_structure = None
+    self._generate_directory_structure()
+
   def _generate_directory_structure(self):
     directory_frames = [
-      self._file_contents[
-        0x80*(frame_number):0x80*(frame_number+1)
+      self._block_raw_data[
+        FRAME_SIZE*(frame_number):FRAME_SIZE*(frame_number+1)
       ] for frame_number in range(1, 16)
     ]
     self._directory_structure = [
       self._parse_directory_frame(frame) for frame in directory_frames
     ]
 
+  def _get_base_256(self, n):
+    return np.array([256**i for i in range(n)])
+
+  def _filter_ascii_chars(self, raw_bytes):
+    filtered_list = [char for char in list(raw_bytes) if 32 <= char <= 126]
+    return ''.join([chr(char) for char in filtered_list])
+
   def _parse_directory_frame(self, raw_data):
-    base_256 = lambda n: np.array([256**i for i in range(n)])
-    name = raw_data[0x0A:0x1E]
-    block_state = np.dot(np.array(list(raw_data[:0x04])), base_256(4))
-    block_size = np.dot(np.array(list(raw_data[0x04:0x08])), base_256(4))/KB
-    next_block = np.dot(np.array(list(raw_data[0x08:0x0A])), base_256(2))
+    block_state = np.dot(np.array(list(raw_data[:0x04])), self._get_base_256(4))
+    block_numbers = np.dot(np.array(list(raw_data[0x04:0x08])), self._get_base_256(4))//BLOCK_SIZE
+    next_block = np.dot(np.array(list(raw_data[0x08:0x0A])), self._get_base_256(2))
+    game_id = self._filter_ascii_chars(raw_data[0x0A:0x0A+12])
+    name = self._filter_ascii_chars(raw_data[0x0A+12:0x1F])
     return {
+      'game_id': game_id,
       'name': name,
       'block_state': block_state,
-      'block_size': block_size,
-      'next_block': next_block,
+      'block_numbers': block_numbers,
+      'next_block_pointer': next_block,
     }
+
 
 class BlockAllocationConstants:
   FIRST_BLOCK = 0x51
